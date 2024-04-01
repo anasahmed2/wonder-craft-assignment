17315c4b65e567d4120a00538df97fd5679768cb
Anas Ahmed <muhammadanas61222@gmail.com>
2024-04-01 16:40:44 +0500

    Add Header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button } from "antd";
+import Image from 'next/image';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className='header-logo'>
+                <h4>Logo</h4>
+            </div>
+            <div className='nav-items'>
+                <Button
+                    className='nav-btn'
+                    type="primary">
+                    FeedBack
+                </Button>
+                <Image alt="nav-icons" src={"/assets/Bell.svg"} width={24} height={24} />
+                <Image alt="nav-icons" src={"/assets/infoCircle.svg"} width={24} height={24} />
+                <Avatar size="large" icon={<UserOutlined />} />
+                <Image alt="nav-icons" src={"/assets/dropdownicon.svg"} width={10} height={10} />
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
